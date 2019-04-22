fce062fcac3809398d8204fd5237bd73149a3c78
z0li <22303427+z0li@users.noreply.github.com>
2019-04-22 13:05:10 +0200

    Setting up the CD pipeline

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ cp README.md master/
 echo "* pushing to master:"
 cd master
 git config user.name "CircleCI"
+git config user.email ${GIT_EMAIL}
 git add -A
 git commit -m "Automated deplyoment job ${CIRCLE_BRANCH} #${CIRCLE_BUILD_NUM} [skip ci]" --allow-empty
 git push origin master
